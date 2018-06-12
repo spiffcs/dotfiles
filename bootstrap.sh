ed6c6a30c6d663d11b6e7fca65db6a6716cb39e2
Christopher Phillips <cphillips918@gmail.com>
2018-06-11 22:46:37 -0400

    update script with better precaution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,6 @@
-git pull && rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+git pull
+read -p "This may overwrite some files in your home directory that already exist. Are you sure you want to proceed? (y/n)" -n 1
+if [[$REPLY =~ ^[Yy]$]]; then
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".osx" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+fi
+echo
