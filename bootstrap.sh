d589d7e82e77ba65fc398df584e09905d1862a35
Christopher Phillips <cphillips918@gmail.com>
2018-06-14 15:04:12 -0400

    Update bootstrap.sh to run from any place

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
+currentdir=`pwd`
+dotdir=`dirname $0`
+cd $dotdir
 git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 fi
-
-source $HOME/.bash_profile
+source "$HOME/.bash_profile"
+cd $currentdir
